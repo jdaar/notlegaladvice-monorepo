864eddf59689792a97e966577246ec05d3387485
jhonatan <jhonatandaar@gmail.com>
2025-04-07 19:10:35 -0500

    chore: actual state before changing machine

--- apps/server/src/app/routes/root.ts
+++ apps/server/src/app/routes/root.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify';
+import { domain } from '@notlegaladvice/domain';
 
 export default async function (fastify: FastifyInstance) {
   fastify.get('/', async function () {
-    return { message: 'Hello API' };
+    return { message: domain() };
   });
 }
